4c33ac87f27b77ef9bf7ae23e069ed9b3e025d39
joker-jonesy <spashmcgrath94@yahoo.com>
2023-06-08 09:27:03 -0400

    sortingExercise

---
+++ objectSort.js
@@ -0,0 +1,24 @@
+// The goal with this app is to build a function that takes the given array and
+// returns an object. The object has an attribute for each different "value", in which those values are an array containing the objects whose value property is that value.
+
+// Example based on below array
+
+// {
+//     '46':[{name: "Billy Joel", value: 46},],
+//     '72':[{name: "Gwen Stefani", value: 72}, {name: "Brandon Flowers", value: 72}]
+// }
+
+const cards = [
+    {
+        name:"Billy Joel",
+        value: 46
+    },
+    {
+        name:"Gwen Stefani",
+        value: 72
+    },
+    {
+        name:"Brandon Flowers",
+        value: 72
+    }
+]
