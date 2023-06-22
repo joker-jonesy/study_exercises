e6e2025baf767a7dc5da49836f9bbff3d5842f12
joker-jonesy <spashmcgrath94@yahoo.com>
2023-06-22 11:40:54 -0400

    Update oreos.js

--- oreos.js
+++ oreos.js
@@ -9,9 +9,32 @@
 
 function createPairs(arr){
     const newArray=[];
+    // look up how to build your own shuffle function
+    const sortedArray= arr.sort(()=>Math.random()-0.5);
+
+    for(let i=0; i<sortedArray.length; i+=2){
+        if(sortedArray[i+3]===undefined){
+            newArray.push(sortedArray.slice(i, i+3))
+            return newArray
+        }else{
+            newArray.push(sortedArray.slice(i, i+2))
+        }
+    }
 
     return newArray;
 }
 
-const items = [1,2,3,4,5,6];
+function createArrays(arr){
+    const newArray=[];
+    const arrays = Math.floor(arr.length/2);
+    for(let i=0; i<arrays; i++){
+        newArray.push([])
+    }
+
+    return newArray;
+}
+
+
+
+const items = [1,2,3,4,5,6,7,8];
 console.log(createPairs(items))
