07ee512b6b7273a5c526e9d17f1c926ade5fa064
joker-jonesy <spashmcgrath94@yahoo.com>
2023-06-22 09:17:09 -0400

    added pairs

--- hexGenerator.js
+++ hexGenerator.js
@@ -1,3 +1,35 @@
 // create two functions, one that takes a hex code and coverts to its RGB value and vis versa
 
 //if you are able to do the above, create an additional function that allows you to take a hexcode and make it more green (not pure green)
+
+function hexToRGB(hex){
+    const r =parseInt(hex.slice(1,3),16);
+    const g =parseInt(hex.slice(3,5),16);
+    const b =parseInt(hex.slice(5,7),16);
+    return {r,g,b};
+}
+
+function valueToHex(c){
+    let hex =c.toString(16);
+    if(hex==="0"){
+        return hex="00"
+    }else{
+        return hex;
+    }
+
+}
+
+function rgbToHex(r,g,b){
+    return (valueToHex(r)+valueToHex(g)+valueToHex(b))
+}
+
+function moreGreen(hex){
+    let rgb = hexToRGB(hex);
+    rgb.g=rgb.g+50;
+    return rgbToHex(rgb.r,rgb.g,rgb.b);
+
+//     255
+}
+
+
+console.log(moreGreen("#ff00ff"))
---
+++ oreos.js
@@ -0,0 +1,17 @@
+// create a function that returns an array, in which in that array, has pairs of items from the array
+// example output: [1,2,3,4] => [[1,2],[3,4]]
+
+//next step, randomize the pairs
+// example output: [1,2,3,4] => [[1,3],[2,4]] or [[4,1],[2,3]]
+
+//if an odd number of items in the initial array, have one pair that is a trio
+// example output: [1,2,3,4,5] => [[1,3],[2,5,4]]
+
+function createPairs(arr){
+    const newArray=[];
+
+    return newArray;
+}
+
+const items = [1,2,3,4,5,6];
+console.log(createPairs(items))
