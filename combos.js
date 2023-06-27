4ddca84d087cae4b42abccc7179d022c69873047
joker-jonesy <spashmcgrath94@yahoo.com>
2023-06-27 09:47:47 -0400

    kermit

---
+++ combos.js
@@ -0,0 +1,5 @@
+// create a function(s) that takes in two main inputs, an array and the number of items in a combo
+// return all the possible combinations of those items from that given array
+// note: you do not need to accomodate for different positions. [1,2,3] and [2,1,3] would count as the same combination
+
+const array = [1,2,3,4,5];
