0a45ff82f17e08597625dfd9fcedcd53cdb53e05
joker-jonesy <spashmcgrath94@yahoo.com>
2023-07-13 11:07:06 -0400

    kermit

--- thisisit.js
+++ thisisit.js
@@ -4,6 +4,40 @@
 // CATCH, when you reference the name directly via the index value of the array to print to the dom, or creating an object where its attribute value is called "name" you must use something else to get it to display
 
 // example: list[0] nor list[1].name cannot be used. You cannot create a different value instead of name, like list[2].cats
-// you can use someting that is not just a string value. Maybe something like...a function?
+// you can use something that is not just a string value. Maybe something like...a function?
 
 const list =[]
+
+function submit(){
+    const value = document.getElementById("nameInput").value;
+    const newItem ={
+        name:value,
+        printName(){
+            return this.name;
+        }
+    }
+    list.push(newItem);
+    // console.log(list)
+    renderList();
+//     itm.name+" "+itm.mn+"....
+}
+
+document.getElementById("submit").addEventListener("click", function(){
+    submit();
+})
+
+function renderList(){
+    document.getElementById("displayList").innerHTML="";
+    list.forEach((itm)=>{
+        const newEle = document.createElement("div");
+        newEle.innerHTML=itm.printName();
+        newEle.addEventListener("click", function(){
+            renderName(itm.printName());
+        })
+        document.getElementById("displayList").appendChild(newEle);
+    })
+}
+
+function renderName(nm){
+    document.getElementById("item").innerHTML=nm;
+}
